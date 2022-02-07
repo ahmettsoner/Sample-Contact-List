58d66aca872670ac9ebc127406e76bf908d74aea
Ahmet Soner <ahmetsoner@morqet.com>
2022-02-07 22:24:45 +0300

    Domain Models moved from wrong project

---
+++ Contact.Service.DomainModels/Contact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Contact.Service.DomainModels
+{
+    public class Contact
+    {
+        public Guid Code { get; set; }
+        public ContactTypes Type { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Contact.Service.DomainModels/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Contact.Service.DomainModels
+{
+    public class Person
+    {
+        public Guid Code { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
