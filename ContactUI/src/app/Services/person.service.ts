561aedf521a8d51154c4ed632ce3831a30d3fe1a
Ahmet Soner <ahmetsoner@morqet.com>
2022-02-08 16:01:00 +0300

    Person service created

--- ContactUI/src/app/Services/person.service.ts
+++ ContactUI/src/app/Services/person.service.ts
@@ -1,9 +1,51 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Person } from '../Models/Person';
+
+const httpOptions={
+  headers: new HttpHeaders({
+    'Content-Type' : 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class PersonService {
 
-  constructor() { }
+
+export class PersonService {
+  serviceUrl:string = 'https://localhost:5001/api/person';
+
+  constructor(private http:HttpClient) { }
+
+  listPersons():Observable<Person[]>{
+    const url = `${this.serviceUrl}`;
+
+    return this.http.get<Person[]>(url, httpOptions);
+  }
+
+  getPersons(code:string):Observable<Person[]>{
+    const url = `${this.serviceUrl}/{code}`;
+
+    return this.http.get<Person[]>(url, httpOptions);
+  }
+
+  addPersons(person:Person):Observable<Person>{
+    const url = `${this.serviceUrl}`;
+
+    return this.http.post<Person>(url, person, httpOptions);
+  }
+
+  updatePersons(person:Person):Observable<Person>{
+    const url = `${this.serviceUrl}/{person.code}`;
+
+    return this.http.put<Person>(url, person, httpOptions);
+  }
+
+  deletePersons(person:Person){
+    const url = `${this.serviceUrl}/{person.code}`;
+
+    this.http.delete(url, httpOptions);
+  }
 }
