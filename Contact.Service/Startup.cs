da336d1e170bef004f6d2429ac41dbcefa3010f9
Ahmet Soner <ahmetsoner@morqet.com>
2022-02-08 12:18:14 +0300

    Web Api Configurated

--- Contact.Service/Startup.cs
+++ Contact.Service/Startup.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contact.Service.API;
+using Contact.Service.Data;
+using Contact.Service.Data.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,18 +27,24 @@ namespace Contact.Service
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
+            services.AddDbContext<ContactServiceDbContext>(o=>{
+                o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddTransient<IContactRepository, ContactRepository>();
+            services.AddTransient<IPersonRepository, PersonRepository>();
+            services.AddTransient<IPersons, Persons>();
+            services.AddTransient<IContacts, Contacts>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Contact.Service", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
