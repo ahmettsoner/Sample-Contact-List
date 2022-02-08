798d17a67b45eb365c7db26f8b3fab2264ef7f92
Ahmet Soner <ahmetsoner@morqet.com>
2022-02-08 14:03:45 +0300

    basic routing implemented

--- ContactUI/src/app/app-routing.module.ts
+++ ContactUI/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent as PersonList } from './Modules/person/list/list.component';
+import { ListComponent as ContactList } from './Modules/contact/list/list.component';
+import { AboutComponent } from './Common/Pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "persons", component:PersonList},
+  {path: "contacts", component:ContactList},
+  {path: "about", component:AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ContactUI/src/app/app.module.ts
+++ ContactUI/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Common/Layout/header/header.component';
+import { AboutComponent } from './Common/Pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
